27030c9d39e3f3eb9e1f7651856f8612ad03d189
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-18 21:49:04 +0530

    Area of circle in C language...

---
+++ Acircle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float radius,area;
+    printf("Enter the radius of circle\n");
+    scanf("%f",& radius);
+    area=3.14*radius*radius ;
+    printf("area of circle %f",area);
+    return 0;
+// In this program find out the area of circle
+}
