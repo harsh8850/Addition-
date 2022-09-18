0fdfa345b230f75fa7f4036fbdc50d90fbf6c953
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-18 21:50:49 +0530

    Area of Rectangle

---
+++ ARectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+    float Area, Leanth ,Width ;
+    printf ("Put the Leanth of Rectangle \n");
+    scanf ("%f",& Leanth);
+    printf("Put the Width of Rectangle \n");
+    scanf ("%f",& Width);
+    printf ("The Multification of Leanth and Width is %f",Leanth* Width);
+    return 0;
+    // In this program find out the area of rectangle
+ 
+}
