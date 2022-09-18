1aea77b49717f90e91140761dbeff5eed3c8c5de
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-18 21:52:43 +0530

    Conversion of Celsius to Fahrenheit in C language

---
+++ CFconversion.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+     float celsius,fahrenheit;
+    printf("put the celsius value \n");
+    scanf("%f",&celsius);
+    fahrenheit=(celsius*1.8)+32;
+    printf("value of fahrenheit %f",fahrenheit);
+    return 0;
+
+}
+// Conversion of celsius degree to fahrenheit degree
