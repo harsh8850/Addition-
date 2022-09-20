7caa44ddf90e35c84807fd93e4f1ee1e02348f32
harsh8850 <113601815+harsh8850@users.noreply.github.com>
2022-09-20 15:09:34 +0530

    Even odd in c language

---
+++ evenodd.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+    int a;
+    printf("Enter the number \n");
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("%d is even\n",a);
+    }
+    else {
+        printf("%d is odd \n",a);
+    }
+    return 0;
+}
+// in this program we can check even and odd number
